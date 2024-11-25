05151340be66ae6b4eb5c9512c05ee2dfb64e701
ivanma777 <ivamaruiz5604@gmail.com>
2024-11-25 16:13:32 +0100

    radio

---
+++ Assets/Script/Dialogos.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogos : ScriptableObject
+{
+    [SerializeField] private string d1;
+    [SerializeField] private string d2;
+    [SerializeField] private string d3;
+    [SerializeField] private string d4;
+    
+}
